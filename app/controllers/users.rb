d7b0fa3df4720ccb252a41316818fcd40f2355ab
Murilo Dal Ri <murilodalri@gmail.com>
2016-04-04 16:37:54 +0100

    user sign up

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,16 +1,16 @@
 class MakersBnB < Sinatra::Base
 
-  post '/users/new' do
-    @user = User.new(email: params[:email],
-                     password: params[:password],
-                     password_confirmation: params[:password_confirmation])
-    if @user.save
-      session[:user_id] = @user.id
-      redirect '/spaces'
-    else
-      flash.next[:errors] = @user.errors.full_messages
-      redirect '/'
-    end
-  end
+  # post '/users/new' do
+  #   @user = User.new(email: params[:email],
+  #                    password: params[:password],
+  #                    password_confirmation: params[:password_confirmation])
+  #   if @user.save
+  #     session[:user_id] = @user.id
+  #     redirect '/spaces'
+  #   else
+  #     flash.next[:errors] = @user.errors.full_messages
+  #     redirect '/'
+  #   end
+  # end
 
 end
---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+require 'BCrypt'
+
+class User
+  include DataMapper::Resource
+  attr_reader :password
+  attr_accessor :password_confirmation
+
+  property :id, Serial
+  property :email, String, format: :email_address, required: true, unique: true
+  property :password_digest, Text
+
+  validates_confirmation_of :password
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+end
