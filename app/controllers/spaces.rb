b1cea15cca95db6998e0bfbfd65afc59dbc77e04
Pete Burch <peteburch223@gmail>
2016-04-05 14:34:36 +0100

    8 functionally complete, pushing so that work can begin on requests

--- app/controllers/spaces.rb
+++ app/controllers/spaces.rb
@@ -2,10 +2,13 @@ class MakersBnB < Sinatra::Base
 
 
   get '/spaces' do
-    @spaces = Space.all(:availabledates.gte => params[:from_date],
-                        :availabledates.lte => params[:to_date],
+    @spaces = Space.all(:availabledates => {:avail_date.gte => params[:from_date], :avail_date.lte => params[:to_date]},
                         :fields => [:name,:description,:price],
                         :order => nil)
+
+
+
+
     erb(:spaces)
   end
 
@@ -14,10 +17,24 @@ class MakersBnB < Sinatra::Base
   end
 
   post '/spaces/new' do
-    Space.create(name: params[:name],
+    date_from = Date.parse(params[:from_date])
+    date_to = Date.parse(params[:to_date])
+
+    days_count = (date_to - date_from).to_i + 1
+
+    space = Space.create(name: params[:name],
                  description: params[:description],
                  price: params[:price])
+
+    days_count.times do |i|
+      space.availabledates << Availabledate.new(avail_date: date_from + i)
+      space.save
+
+    end
+
     redirect '/spaces'
   end
+
+
 
 end
--- spec/features/list_a_space_spec.rb
+++ spec/features/list_a_space_spec.rb
@@ -3,8 +3,8 @@ feature 'Spaces' do
   let(:name_content){"Pete's grotty gaff"}
   let(:description_content){"Quite smelly, but nice view"}
   let(:price_content){'99.99'}
-  let(:from_date){'03/03/16'}
-  let(:to_date){'01/04/16'}
+  let(:from_date){'03/03/2016'}
+  let(:to_date){'01/04/2016'}
 
   let(:name_content2){"Shane's gaff"}
   let(:description_content2){"Quite pretty, but nice view"}
@@ -29,13 +29,22 @@ feature 'Spaces' do
     fill_in('spaceName', with: name_content)
     fill_in('spaceDescription', with: description_content)
     fill_in('spacePrice', with: price_content)
+    fill_in('fromDate', with: from_date)
+    fill_in('toDate', with: to_date)
     click_button('List my Space')
 
     click_button('List a Space')
     fill_in('spaceName', with: name_content2)
     fill_in('spaceDescription', with: description_content2)
     fill_in('spacePrice', with: price_content2)
+    fill_in('fromDate', with: from_date)
+    fill_in('toDate', with: to_date)
     click_button('List my Space')
+
+    fill_in('fromDate', with: from_date)
+    fill_in('toDate', with: to_date)
+    click_button('List Spaces')
+
 
     expect(page).to have_content(name_content)
     expect(page).to have_content(description_content)
