d587616841489a1a44c3023b5f305753fcf0d997
Murilo Dal Ri <murilodalri@gmail.com>
2016-04-05 12:27:36 +0100

    signing out feature implemented

---
+++ spec/features/signing_out_spec.rb
@@ -0,0 +1,12 @@
+feature 'Signing out' do
+  scenario 'when logged in' do
+    sign_up
+    click_button 'Log out'
+    expect(page).not_to have_content 'test@test.com'
+  end
+
+  scenario 'when logged out' do
+    visit '/'
+    expect(page).not_to have_content 'Log out'
+  end
+end
