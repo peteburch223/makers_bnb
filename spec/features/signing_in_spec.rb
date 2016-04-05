ce650df52608b460b61cc042b934648ddbb613a4
Simon Glancy <simonglancy@Simons-MacBook-Pro-6.local>
2016-04-05 11:45:13 +0100

    user story complete -2

--- spec/features/signing_in_spec.rb
+++ spec/features/signing_in_spec.rb
@@ -7,9 +7,15 @@ feature 'Signing in' do
     expect(page).to have_content('test@test.com')
   end
 
-  it 'allows a user to sign in' do
+  it 'does not allow a user to sign in with incorrect password' do
     sign_up
     sign_in(password: 789101)
+    expect(page).to have_content('The email or password is incorrect')
+  end
+
+  it 'does not allow a user to sign in with incorrect email' do
+    sign_up
+    sign_in(email: 'twats@email.com')
     expect(page).to have_content('The email or password is incorrect')
   end
 
