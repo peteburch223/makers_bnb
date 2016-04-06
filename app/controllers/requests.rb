31421565d1ac9ee96ac5145978065e37ded032a4
Pete Burch <peteburch223@gmail>
2016-04-06 18:47:20 +0100

    displaying correct data in requests review screen, needs refactoring

--- app/controllers/requests.rb
+++ app/controllers/requests.rb
@@ -7,7 +7,7 @@ class MakersBnB < Sinatra::Base
     #----------------- CALENDAR INPUT TO COME -------------------------------
     availabledate.each{|a_date| Request.create(user_id: current_user.id,
                                                availabledate_id: a_date.id,
-                                               status: "open")}
+                                               status: Helpers::NOT_CONFIRMED)}
     redirect '/requests'
   end
 
@@ -15,11 +15,28 @@ class MakersBnB < Sinatra::Base
 
     redirect '/spaces' unless current_user
 
-    @space_requests_made = Space.all(availabledates: { requests: { user_id: current_user.id } })
-    @space_requests_received = Space.all(user_id: current_user.id)
-    @space_requests_received.reject!{|space| space.availabledates.requests.empty?}
+    # need to ensure that multiple requests to the same space are considered separately
+    space_requests_made = Space.all(availabledates: { requests: { user_id: current_user.id } })
+    @space_requests_made = prepare_request_display(space_requests_made)
 
+
+    space_requests_received = Space.all(user_id: current_user.id)
+    space_requests_received.reject!{|space| space.availabledates.requests.empty?}
+    @space_requests_received = prepare_request_display(space_requests_received)
     erb(:requests)
+  end
+
+
+  def prepare_request_display(requests)
+    return_value = []
+    requests.each do |space|
+      result = []
+      result << space
+      result << space.availabledates.requests.first.status
+      result << space.availabledates.first.avail_date.strftime('%d %m %Y') + (' - ' + space.availabledates.last.avail_date.strftime('%d %m %Y') if space.availabledates.count > 1)
+      return_value << result
+    end
+    return_value
   end
 
 
