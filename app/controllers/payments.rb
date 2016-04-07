87bade388a7bf93589c967798fcf9204923b1741
Murilo Dal Ri <murilodalri@gmail.com>
2016-04-07 22:45:43 +0100

    test

--- app/controllers/payments.rb
+++ app/controllers/payments.rb
@@ -1,11 +1,10 @@
 class MakersBnB < Sinatra::Base
-
   get '/payments' do
     erb :payment
   end
 
   post '/payments/new' do
-    Stripe.api_key = "sk_test_gW9ngr4oHcQm55aRLV4rn3it"
+    Stripe.api_key = 'sk_test_gW9ngr4oHcQm55aRLV4rn3it'
 
     # Get the credit card details submitted by the form
     token = params[:stripeToken]
@@ -13,10 +12,10 @@ class MakersBnB < Sinatra::Base
     # Create the charge on Stripe's servers - this will charge the user's card
     begin
       charge = Stripe::Charge.create(
-        :amount => 1000, # amount in cents, again
-        :currency => "gbp",
-        :source => token,
-        :description => "Example charge"
+        amount: 1000, # amount in cents, again
+        currency: 'gbp',
+        source: token,
+        description: 'Example charge'
       )
     rescue Stripe::CardError => e
       # The card has been declined
