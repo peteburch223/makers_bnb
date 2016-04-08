827bdf6e6e90a8aefa1ae90a863323a3a763c653
Murilo Dal Ri <murilodalri@gmail.com>
2016-04-08 09:55:04 +0100

    dsfdsf

--- app/controllers/payments.rb
+++ app/controllers/payments.rb
@@ -8,6 +8,9 @@ class MakersBnB < Sinatra::Base
 
     # Get the credit card details submitted by the form
     token = params[:stripeToken]
+    # amount = params[:amount]
+    
+
 
     # Create the charge on Stripe's servers - this will charge the user's card
     begin
